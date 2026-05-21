1ac8c9d1577ebc89b86e2c65c0fa47f614693038
rohannuthi7 <rohannuthi07@gmail.com>
2026-05-21 04:36:03 -0500

    Make project deployable

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -10,11 +11,16 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // middleware to parse JSON bodies
-app.use(cors({
-    origin: "http://localhost:5173",  // allow requests from this origin
-}));
+if (process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173",  // allow requests from this origin
+        })
+    );
+}
 app.use(express.json()); // this middleware will parse the JSON bodies: req.body
 app.use(rateLimiter); // apply rate limiting middleware to all routes
 
@@ -27,6 +33,14 @@ app.use((req, res, next) => {
 */
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
